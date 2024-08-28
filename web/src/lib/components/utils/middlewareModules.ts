8510a768d19154c54c1f94314aeb7536ad0d3cfa
d34dscene <xddd.governeurs@slmail.me>
2024-08-28 17:41:05 +0200

    small await fix

--- web/src/lib/components/utils/middlewareModules.ts
+++ web/src/lib/components/utils/middlewareModules.ts
@@ -2,31 +2,31 @@ import type { Middleware } from '$lib/types/middlewares';
 import type { SvelteComponent } from 'svelte';
 
 export const MiddlewareForms = {
-	addPrefix: (await import('$lib/components/forms/addPrefix.svelte')).default,
-	stripPrefix: (await import('$lib/components/forms/stripPrefix.svelte')).default,
-	stripPrefixRegex: (await import('$lib/components/forms/stripPrefixRegex.svelte')).default,
-	replacePath: (await import('$lib/components/forms/replacePath.svelte')).default,
-	replacePathRegex: (await import('$lib/components/forms/replacePathRegex.svelte')).default,
-	chain: (await import('$lib/components/forms/chain.svelte')).default,
-	ipAllowList: (await import('$lib/components/forms/ipAllowList.svelte')).default,
-	headers: (await import('$lib/components/forms/headers.svelte')).default,
-	errors: (await import('$lib/components/forms/errorPage.svelte')).default,
-	rateLimit: (await import('$lib/components/forms/rateLimit.svelte')).default,
-	redirectRegex: (await import('$lib/components/forms/redirectRegex.svelte')).default,
-	redirectScheme: (await import('$lib/components/forms/redirectScheme.svelte')).default,
-	basicAuth: (await import('$lib/components/forms/basicAuth.svelte')).default,
-	digestAuth: (await import('$lib/components/forms/digestAuth.svelte')).default,
-	forwardAuth: (await import('$lib/components/forms/forwardAuth.svelte')).default,
-	inFlightReq: (await import('$lib/components/forms/inFlightReq.svelte')).default,
-	buffering: (await import('$lib/components/forms/buffering.svelte')).default,
-	circuitBreaker: (await import('$lib/components/forms/circuitBreaker.svelte')).default,
-	compress: (await import('$lib/components/forms/compress.svelte')).default,
-	// passTLSClientCert: (await import('$lib/components/forms/passTLSClientCert.svelte')).default,
-	retry: (await import('$lib/components/forms/retry.svelte')).default,
+	addPrefix: import('$lib/components/forms/addPrefix.svelte'),
+	stripPrefix: import('$lib/components/forms/stripPrefix.svelte'),
+	stripPrefixRegex: import('$lib/components/forms/stripPrefixRegex.svelte'),
+	replacePath: import('$lib/components/forms/replacePath.svelte'),
+	replacePathRegex: import('$lib/components/forms/replacePathRegex.svelte'),
+	chain: import('$lib/components/forms/chain.svelte'),
+	ipAllowList: import('$lib/components/forms/ipAllowList.svelte'),
+	headers: import('$lib/components/forms/headers.svelte'),
+	errors: import('$lib/components/forms/errorPage.svelte'),
+	rateLimit: import('$lib/components/forms/rateLimit.svelte'),
+	redirectRegex: import('$lib/components/forms/redirectRegex.svelte'),
+	redirectScheme: import('$lib/components/forms/redirectScheme.svelte'),
+	basicAuth: import('$lib/components/forms/basicAuth.svelte'),
+	digestAuth: import('$lib/components/forms/digestAuth.svelte'),
+	forwardAuth: import('$lib/components/forms/forwardAuth.svelte'),
+	inFlightReq: import('$lib/components/forms/inFlightReq.svelte'),
+	buffering: import('$lib/components/forms/buffering.svelte'),
+	circuitBreaker: import('$lib/components/forms/circuitBreaker.svelte'),
+	compress: import('$lib/components/forms/compress.svelte'),
+	// passTLSClientCert: import('$lib/components/forms/passTLSClientCert.svelte')),
+	retry: import('$lib/components/forms/retry.svelte'),
 
 	// TCP-specific
-	inFlightConn: (await import('$lib/components/forms/inFlightConn.svelte')).default,
-	tcpIpAllowList: (await import('$lib/components/forms/tcpIpAllowList.svelte')).default
+	inFlightConn: import('$lib/components/forms/inFlightConn.svelte'),
+	tcpIpAllowList: import('$lib/components/forms/tcpIpAllowList.svelte')
 };
 
 export const LoadMiddlewareForm = async (
@@ -36,5 +36,5 @@ export const LoadMiddlewareForm = async (
 		(key) => key.toLowerCase() === mw.type?.toLowerCase()
 	) as keyof typeof MiddlewareForms | undefined;
 
-	return moduleKey ? MiddlewareForms[moduleKey] : null;
+	return moduleKey ? (await MiddlewareForms[moduleKey]).default : null;
 };
