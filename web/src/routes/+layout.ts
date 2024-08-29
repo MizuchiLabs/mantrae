9e3964663005bfc8bb499721151af528f0892713
d34dscene <xddd.governeurs@slmail.me>
2024-08-29 20:01:46 +0200

    fix trailing slash

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -4,6 +4,7 @@ import type { LayoutLoad } from './$types';
 
 export const ssr = false;
 export const prerender = true;
+export const trailingSlash = 'always';
 
 export const load: LayoutLoad = async ({ fetch, url }) => {
 	const token = localStorage.getItem('token');
