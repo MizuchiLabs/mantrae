016439c3936726bcf3d5df08592b4dc39600cc8e
NCRoxas <xddd.governeurs@slmail.me>
2024-09-05 03:07:24 +0200

    fix chain display

--- web/src/lib/components/forms/chain.svelte
+++ web/src/lib/components/forms/chain.svelte
@@ -4,6 +4,7 @@
 	import * as Select from '$lib/components/ui/select';
 	import type { Selected } from 'bits-ui';
 	import { middlewares } from '$lib/api';
+	import ArrayInput from '../ui/array-input/array-input.svelte';
 
 	export let middleware: Middleware;
 	export let disabled = false;
@@ -19,21 +20,30 @@
 </script>
 
 {#if middleware.chain}
-	<div class="grid grid-cols-4 items-center gap-4">
-		<Label for="middlewares" class="text-right">Middlewares</Label>
-		<div class="col-span-3 space-y-2">
-			<Select.Root multiple selected={selectedMiddlewares} onSelectedChange={changeMiddlewares}>
-				<Select.Trigger>
-					<Select.Value placeholder="Select middlewares to chain" />
-				</Select.Trigger>
-				<Select.Content class="no-scrollbar max-h-[300px] overflow-y-auto">
-					{#each $middlewares as m}
-						<Select.Item value={m.name} label={m.name}>
-							{m.name}
-						</Select.Item>
-					{/each}
-				</Select.Content>
-			</Select.Root>
+	{#if disabled}
+		<ArrayInput
+			items={middleware.chain.middlewares}
+			label="Middlewares"
+			placeholder=""
+			disabled={true}
+		/>
+	{:else}
+		<div class="grid grid-cols-4 items-center gap-4">
+			<Label for="middlewares" class="text-right">Middlewares</Label>
+			<div class="col-span-3 space-y-2">
+				<Select.Root multiple selected={selectedMiddlewares} onSelectedChange={changeMiddlewares}>
+					<Select.Trigger>
+						<Select.Value placeholder="Select middlewares to chain" />
+					</Select.Trigger>
+					<Select.Content class="no-scrollbar max-h-[300px] overflow-y-auto">
+						{#each $middlewares as m}
+							<Select.Item value={m.name} label={m.name}>
+								{m.name}
+							</Select.Item>
+						{/each}
+					</Select.Content>
+				</Select.Root>
+			</div>
 		</div>
-	</div>
+	{/if}
 {/if}
