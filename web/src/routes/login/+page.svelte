ea32b29a592c0a6f327c76f7524ad9ae323537c5
d34dscene <xddd.governeurs@slmail.me>
2024-10-02 14:45:02 +0200

    add show pass

--- web/src/routes/login/+page.svelte
+++ web/src/routes/login/+page.svelte
@@ -4,9 +4,11 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { login, loggedIn } from '$lib/api';
+	import { Eye, EyeOff } from 'lucide-svelte';
 
 	let username = '';
 	let password = '';
+	let showPassword = false;
 
 	const handleSubmit = async () => {
 		await login(username, password);
@@ -32,12 +34,30 @@
 				</div>
 				<div class="flex flex-col space-y-1.5">
 					<Label for="password">Password</Label>
-					<Input id="password" type="password" bind:value={password} />
+					<div class="flex flex-row items-center justify-end gap-1">
+						{#if showPassword}
+							<Input id="password" type="text" bind:value={password} />
+						{:else}
+							<Input id="password" type="password" bind:value={password} />
+						{/if}
+						<Button
+							variant="ghost"
+							size="icon"
+							class="absolute hover:bg-transparent hover:text-red-400"
+							on:click={() => (showPassword = !showPassword)}
+						>
+							{#if showPassword}
+								<Eye size="1rem" />
+							{:else}
+								<EyeOff size="1rem" />
+							{/if}
+						</Button>
+					</div>
 				</div>
-			</div>
-			<div class="mt-4 flex flex-col">
-				<Button type="submit" on:click={handleSubmit}>Login</Button>
-			</div>
-		</Card.Content>
+				<div class="mt-4 flex flex-col">
+					<Button type="submit" on:click={handleSubmit}>Login</Button>
+				</div>
+			</div></Card.Content
+		>
 	</Card.Root>
 {/if}
