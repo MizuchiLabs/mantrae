e9c64dd409263588174542ddba6bb7cf3d65beb7
d34dscene <d34dscene@pm.me>
2025-09-18 12:12:55 +0200

    feat: add servers transport and sticky cookie to services

--- web/src/lib/components/forms/HTTPServiceForm.svelte
+++ web/src/lib/components/forms/HTTPServiceForm.svelte
@@ -2,60 +2,62 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
 	import { type Service } from '$lib/gen/mantrae/v1/service_pb';
-	import type { Service as HTTPService } from '$lib/gen/zen/traefik-schemas';
+	import {
+		type Cookie,
+		type Service as HTTPService,
+		type Server,
+		type ServersLoadBalancer
+	} from '$lib/gen/zen/traefik-schemas';
 	import { Plus, Trash } from '@lucide/svelte';
 	import { marshalConfig } from '$lib/types';
 	import CustomSwitch from '../ui/custom-switch/custom-switch.svelte';
+	import { serversTransportClient } from '$lib/api';
+	import { profile } from '$lib/stores/profile';
 
 	interface Props {
 		service: Service;
 	}
 	let { service = $bindable() }: Props = $props();
 
-	let config = $state({} as HTTPService);
+	let servers = $state([{ url: '' }] as Server[]);
+	let passHostHeader = $state(true);
+	let serversTransport = $state('');
+	let sticky = $state(false);
+	let cookie = $state({} as Cookie);
+
+	function updateConfig() {
+		const config = {} as HTTPService;
+		config.loadBalancer = {} as ServersLoadBalancer;
+		config.loadBalancer.servers = servers;
+		config.loadBalancer.passHostHeader = passHostHeader;
+		config.loadBalancer.sticky = sticky ? { cookie } : undefined;
+		config.loadBalancer.serversTransport = serversTransport;
+		service.config = marshalConfig(config);
+	}
 
 	$effect(() => {
 		if (service.config) {
-			config = service.config as HTTPService;
-		}
-		if (!config.loadBalancer) config.loadBalancer = { passHostHeader: true };
-		if (!config.loadBalancer.servers) config.loadBalancer.servers = [];
-		if (config.loadBalancer.servers.length === 0) {
-			config.loadBalancer.servers = [{ url: '' }];
+			let config = service.config as HTTPService;
+			servers = config.loadBalancer?.servers || [];
+			passHostHeader = config.loadBalancer?.passHostHeader ?? true;
+			sticky = config.loadBalancer?.sticky?.cookie !== undefined;
+			cookie = config.loadBalancer?.sticky?.cookie ?? {};
+			serversTransport = config.loadBalancer?.serversTransport ?? '';
 		}
 	});
 </script>
 
 <div class="flex flex-col gap-3">
-	<div class="flex items-center justify-between rounded-lg border p-3">
-		<div class="space-y-1">
-			<Label class="flex items-center gap-1 text-sm">Pass Host Header</Label>
-			<p class="text-xs text-muted-foreground">Forward client host header to server</p>
-		</div>
-		<CustomSwitch
-			checked={config.loadBalancer?.passHostHeader ?? true}
-			onCheckedChange={(value) => {
-				if (!config.loadBalancer) config.loadBalancer = { passHostHeader: true };
-				config.loadBalancer.passHostHeader = value;
-				service.config = marshalConfig(config);
-			}}
-			size="md"
-		/>
-	</div>
-
 	<div class="flex flex-col gap-2">
 		<Label for="servers">Server Endpoints</Label>
-		{#each config.loadBalancer?.servers || [] as server, i (i)}
+		{#each servers || [] as server, i (i)}
 			<div class="flex gap-2">
 				<Input
 					type="text"
-					value={server.url}
-					oninput={(e) => {
-						let input = e.target as HTMLInputElement;
-						server.url = input.value;
-						service.config = marshalConfig(config);
-					}}
+					bind:value={server.url}
+					oninput={updateConfig}
 					placeholder="http://127.0.0.1:8080"
 				/>
 				<Button
@@ -65,10 +67,7 @@
 					class="text-red-500"
 					onclick={() => {
 						if (i === 0) return;
-						if (!config.loadBalancer) config.loadBalancer = { passHostHeader: true };
-						if (!config.loadBalancer.servers) config.loadBalancer.servers = [];
-						config.loadBalancer.servers = config.loadBalancer.servers.filter((_, j) => j !== i);
-						service.config = marshalConfig(config);
+						servers = servers.filter((_, j) => j !== i);
 					}}
 				>
 					<Trash />
@@ -80,14 +79,66 @@
 		type="button"
 		variant="outline"
 		class="w-full"
-		onclick={() => {
-			if (!config.loadBalancer) config.loadBalancer = { passHostHeader: true };
-			if (!config.loadBalancer.servers) config.loadBalancer.servers = [];
-			config.loadBalancer.servers = [...config.loadBalancer.servers, { url: '' }];
-			service.config = marshalConfig(config);
-		}}
+		onclick={() => (servers = [...servers, { url: '' }])}
 	>
 		<Plus />
 		Add Server
 	</Button>
+
+	<div class="flex items-center justify-between rounded-lg border p-3">
+		<div class="space-y-1">
+			<Label class="flex items-center gap-1 text-sm">Pass Host Header</Label>
+			<p class="text-xs text-muted-foreground">Forward client host header to server</p>
+		</div>
+		<CustomSwitch bind:checked={passHostHeader} onCheckedChange={updateConfig} size="md" />
+	</div>
+
+	<div class="grid grid-cols-4 items-center gap-3 rounded-lg border p-3">
+		<div class="col-span-3 space-y-1">
+			<Label class="flex items-center gap-1 text-sm">Sticky Cookie</Label>
+			<p class="text-xs text-muted-foreground">
+				Defines a Set-Cookie header is set on the initial response to let the client know which
+				server handles the first response.
+			</p>
+		</div>
+		<div class="justify-self-end">
+			<CustomSwitch bind:checked={sticky} onCheckedChange={updateConfig} size="md" />
+		</div>
+	</div>
+
+	{#if sticky}
+		<div class="flex flex-col gap-2">
+			<Label for="cookie-name">Cookie Name</Label>
+			<Input
+				id="cookie-name"
+				type="text"
+				bind:value={cookie.name}
+				oninput={updateConfig}
+				placeholder="session"
+			/>
+		</div>
+	{/if}
+
+	{#await serversTransportClient.listServersTransports({ profileId: profile.id }) then value}
+		<div class="flex flex-col gap-2">
+			<Label for="servers-transport">Servers Transport</Label>
+			<Select.Root type="single" bind:value={serversTransport} onValueChange={updateConfig}>
+				<Select.Trigger class="w-full">
+					<span class="truncate text-left">
+						{serversTransport || 'Select servers transport'}
+					</span>
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value="">
+						<span class="truncate">Select servers transport</span>
+					</Select.Item>
+					{#each value.serversTransports || [] as transport (transport.id)}
+						<Select.Item value={transport.name}>
+							<span class="truncate">{transport.name}</span>
+						</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+		</div>
+	{/await}
 </div>
