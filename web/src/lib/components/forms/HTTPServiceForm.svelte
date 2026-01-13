f44fe8e9064551f736ea358c59c36640b45bdf5b
hetsen <mikael.isaksson@tankbar.com>
2026-01-13 11:27:35 +0100

    Add healthcheck to HTTPServiceForm.svelte

--- web/src/lib/components/forms/HTTPServiceForm.svelte
+++ web/src/lib/components/forms/HTTPServiceForm.svelte
@@ -8,13 +8,47 @@
 		type Cookie,
 		type Service as HTTPService,
 		type Server,
-		type ServersLoadBalancer
+		type ServersLoadBalancer, ServerHealthCheckSchema
 	} from '$lib/gen/zen/traefik-schemas';
-	import { Plus, Trash } from '@lucide/svelte';
-	import { marshalConfig } from '$lib/types';
+	import { Plus, Trash, BrushCleaning } from '@lucide/svelte';
+	import {marshalConfig, unmarshalConfig} from '$lib/types';
 	import CustomSwitch from '../ui/custom-switch/custom-switch.svelte';
 	import { serversTransportClient } from '$lib/api';
 	import { profile } from '$lib/stores/profile';
+	import {string} from "zod";
+	import {Textarea} from "$lib/components/ui/textarea";
+
+	const getInputType = (key: keyof HealthCheckType) => {
+		const typeMap: Record<keyof HealthCheckType> = {
+			scheme: 'text',
+			mode: 'text',
+			path: 'text',
+			method: 'text',
+			status: 'number',
+			port: 'number',
+			interval: 'text',
+			unhealthyInterval: 'text',
+			timeout: 'text',
+			hostname: 'text',
+			followRedirects: 'checkbox',
+		};
+		return typeMap[key];
+	}
+
+	const healthcheckPlaceholders = {
+		scheme: "http",
+		mode: "http",
+		path: "/health",
+		method: "GET",
+		status: "200",
+		port: "80",
+		interval: "10s",
+		unhealthyInterval: "3s",
+		timeout: "2s",
+		hostname: "example.internal",
+		followRedirects: "true",
+		headers: "X-Health-Check: true"
+	};
 
 	interface Props {
 		service: Service;
@@ -26,6 +60,8 @@
 	let serversTransport = $state('');
 	let sticky = $state(false);
 	let cookie = $state({} as Cookie);
+	let healthCheck = $state({});
+	let headers = $state({ key: '' });
 
 	function updateConfig() {
 		const config = {} as HTTPService;
@@ -34,7 +70,18 @@
 		config.loadBalancer.passHostHeader = passHostHeader;
 		config.loadBalancer.sticky = sticky ? { cookie } : undefined;
 		config.loadBalancer.serversTransport = serversTransport;
+		config.loadBalancer.healthCheck = healthCheck ?? {};
+		config.loadBalancer.healthCheck.headers = headers ?? {};
+
+		Object.entries(healthCheck).forEach(([key, value]) => {
+			if (value === "") {
+				delete healthCheck[key];
+			}
+		});
+
 		service.config = marshalConfig(config);
+
+
 	}
 
 	$effect(() => {
@@ -45,8 +92,22 @@
 			sticky = config.loadBalancer?.sticky?.cookie !== undefined;
 			cookie = config.loadBalancer?.sticky?.cookie ?? {};
 			serversTransport = config.loadBalancer?.serversTransport ?? '';
+			healthCheck = config.loadBalancer?.healthCheck ?? {};
+			headers = config.loadBalancer?.healthCheck?.headers ?? {};
 		}
 	});
+
+	function capitalizeFirstLetter(string) {
+		return string.replace(/^./, string[0].toUpperCase())
+	}
+
+	function getHeaderKeyNames() {
+		return Object.keys(headers);
+	}
+
+
+
+
 </script>
 
 <div class="flex flex-col gap-3">
@@ -55,17 +116,17 @@
 		{#each servers || [] as server, i (i)}
 			<div class="flex gap-2">
 				<Input
-					type="text"
-					bind:value={server.url}
-					oninput={updateConfig}
-					placeholder="http://127.0.0.1:8080"
+						type="text"
+						bind:value={server.url}
+						oninput={updateConfig}
+						placeholder="http://127.0.0.1:8080"
 				/>
 				<Button
-					variant="ghost"
-					size="icon"
-					type="button"
-					class="text-red-500"
-					onclick={() => {
+						variant="ghost"
+						size="icon"
+						type="button"
+						class="text-red-500"
+						onclick={() => {
 						if (i === 0) return;
 						servers = servers.filter((_, j) => j !== i);
 					}}
@@ -76,10 +137,10 @@
 		{/each}
 	</div>
 	<Button
-		type="button"
-		variant="outline"
-		class="w-full"
-		onclick={() => (servers = [...servers, { url: '' }])}
+			type="button"
+			variant="outline"
+			class="w-full"
+			onclick={() => (servers = [...servers, { url: '' }])}
 	>
 		<Plus />
 		Add Server
@@ -93,6 +154,110 @@
 		<CustomSwitch bind:checked={passHostHeader} onCheckedChange={updateConfig} size="md" />
 	</div>
 
+	<div class=" items-center justify-between rounded-lg border p-3">
+		<Label class="flex items-center text-1xl">Healthcheck</Label>
+		<p class="text-xs text-muted-foreground pb-3">Monitors service health</p>
+
+		<div class="flex items-center justify-between rounded-lg border p-3">
+				<div class="space-y-1">
+					<Label class="flex items-center gap-1 text-xs">Clear Healthcheck</Label>
+				</div>
+				<Button
+						variant="ghost"
+						size="default"
+						type="button"
+						class="text-orange-300"
+
+						onclick={()=> {healthCheck = {}; headers = {}; updateConfig();}}
+				>
+					<BrushCleaning />
+				</Button>
+			</div>
+
+		{#each Object.keys(ServerHealthCheckSchema.shape) as key}
+			{@const inputType = getInputType(key)}
+
+			{#if inputType !== 'checkbox' && key !== 'headers'}
+				<div class="flex flex-col gap-2 pt-2">
+					<Label for="cookie-name">{capitalizeFirstLetter(key)}</Label>
+					<Input
+							id="cookie-name"
+							type={inputType}
+							bind:value={healthCheck[key]}
+							oninput={updateConfig}
+							placeholder="{healthcheckPlaceholders[key]}"
+					/>
+				</div>
+			{:else if inputType === 'checkbox' && key !== 'headers'}
+				<div class="flex flex-col gap-2 pt-2 pb-2">
+					<Label for="cookie-name">{capitalizeFirstLetter(key)}</Label>
+					<CustomSwitch bind:checked={healthCheck[key]} onCheckedChange={updateConfig} size="md" />
+				</div>
+			{:else}
+				<Label class="pt-2 pb-2" for="cookie-name">{capitalizeFirstLetter(key)}</Label>
+				{#each Object.entries(headers) || {} as [keyName, value], i (i)}
+					{@const nameofKey = getHeaderKeyNames()}
+					<div class="flex gap-4 pb-2">
+						<div class="col-span-3 space-y-1">
+
+							<Input
+									type="text"
+									bind:value={nameofKey[i]}
+									oninput={(e) => {
+										const newKey = e.target.value;
+										if (newKey === keyName) return;
+
+										headers = {
+											...Object.fromEntries(
+												Object.entries(headers).filter(([k]) => k !== keyName)
+											),
+											[newKey]: value
+										};
+										updateConfig();
+									}}
+
+							/>
+						</div>
+						<div class="col-span-3 space-y-1">
+							<Input
+									type="text"
+									bind:value={headers[keyName]}
+									oninput={updateConfig}
+									}}
+							/>
+						</div>
+						<Button
+								variant="ghost"
+								size="icon"
+								type="button"
+								class="text-red-500"
+								onclick={() => { delete headers[keyName]; updateConfig();}}
+						>
+							<Trash />
+						</Button>
+					</div>
+
+				{/each}
+				<Button
+						type="button"
+						variant="outline"
+						class="w-full"
+						onclick={
+
+						() => {
+							const randomString = crypto.randomUUID();
+							headers = {...headers, [randomString]: 'value' };
+							updateConfig();
+						}
+
+						}
+				>
+					<Plus />
+					Add Header
+				</Button>
+			{/if}
+		{/each}
+	</div>
 	<div class="grid grid-cols-4 items-center gap-3 rounded-lg border p-3">
 		<div class="col-span-3 space-y-1">
 			<Label class="flex items-center gap-1 text-sm">Sticky Cookie</Label>
@@ -110,11 +275,11 @@
 		<div class="flex flex-col gap-2">
 			<Label for="cookie-name">Cookie Name</Label>
 			<Input
-				id="cookie-name"
-				type="text"
-				bind:value={cookie.name}
-				oninput={updateConfig}
-				placeholder="session"
+					id="cookie-name"
+					type="text"
+					bind:value={cookie.name}
+					oninput={updateConfig}
+					placeholder="session"
 			/>
 		</div>
 	{/if}
@@ -124,9 +289,9 @@
 			<Label for="servers-transport">Servers Transport</Label>
 			<Select.Root type="single" bind:value={serversTransport} onValueChange={updateConfig}>
 				<Select.Trigger class="w-full">
-					<span class="truncate text-left">
-						{serversTransport || 'Select servers transport'}
-					</span>
+                    <span class="truncate text-left">
+                        {serversTransport || 'Select servers transport'}
+                    </span>
 				</Select.Trigger>
 				<Select.Content>
 					<Select.Item value="">
